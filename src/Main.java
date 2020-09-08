880ad51002f970fa8021503b4a2d8964d1dbb4fd
William Nylund <william.nylund@gmail.com>
2020-09-08 17:02:31 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world, its ya boi scarce here");
+        System.out.println("Yo");
+
     }
 }
