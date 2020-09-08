a54d241916cb91037d71b33bdac948ca82f68dd5
hmartola <64286599+hmartola@users.noreply.github.com>
2020-09-08 17:19:27 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello world, its ya boi scarce here");
+        for (int i = 0; i < 69; i++){
+            System.out.println("chip me daddy");
+        }
     }
 }
