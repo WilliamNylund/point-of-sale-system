6d7237ee860bd17cc945e4355a352032eca5ff5c
rkantero <jrkantero@gmail.com>
2020-09-08 20:08:04 +0300

    slightly modify customermonitor

--- src/view/CustomerMonitor.java
+++ src/view/CustomerMonitor.java
@@ -24,7 +24,7 @@ public class CustomerMonitor extends Application {
 
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Well hello to you, too!");
             }
         });
 
