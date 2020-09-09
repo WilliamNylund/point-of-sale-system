09cf108165711c397dad25468145f40032ad1620
rkantero <jrkantero@gmail.com>
2020-09-09 16:52:14 +0300

    add label to customerScreen window & fxids to all elements

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("view/CustomerScreen.fxml")));
+        primaryStage.setTitle("Customer");
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
