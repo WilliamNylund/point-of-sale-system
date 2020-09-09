1a148f350ca5e76916d9555236b6479cf0ad299f
William Nylund <william.nylund@gmail.com>
2020-09-09 16:11:48 +0300

    add Item class & implement methods in CSC.java

---
+++ src/model/Item.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Item {
+
+    private String name;
+    private int id;
+    private double price;
+
+    public Item(String name, double price){
+        this.setName(name);
+        this.setPrice(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
