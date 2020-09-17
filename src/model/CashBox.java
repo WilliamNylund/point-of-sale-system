b4c2dbb4195b9e50c7ee3c8a300362343e748549
William Nylund <william.nylund@gmail.com>
2020-09-17 14:20:17 +0300

    fix open and getStatus methods in CashBox.java

--- src/model/CashBox.java
+++ src/model/CashBox.java
@@ -4,36 +4,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 public class CashBox {
 
-    public void open() throws IOException {
-        System.out.println("opening CashBox!");
-        URL url = new URL("http://localhost:9001/cashbox/open");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("POST");
-        con.setDoOutput(true);
-    }
-    public String getStatus() throws IOException, InterruptedException {
-
-        URL url = new URL("http://localhost:9001/cashbox/status");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+    public void open() {
+        try{
+            System.out.println("opening CashBox!");
+            URL url = new URL("http://localhost:9001/cashbox/open");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.getInputStream();
+            System.out.println("CashBox opened!");
+        } catch (IOException e){
+            System.out.println("something went terrible wrong in CashBox.open() :(((");
         }
-        in.close();
-
-        con.disconnect();
-        System.out.println(content);
-
-        return "status";
+    }
+    public String getStatus() {
+        try{
+            URL url = new URL("http://localhost:9001/cashbox/status");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+            return content.toString();
+        } catch(IOException e){
+            return "something went terrible wrong in CashBox.getStatus() :(((";
+        }
     }
 }
