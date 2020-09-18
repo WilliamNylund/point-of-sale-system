e24c567d68afe7fd2846f609e5166589a3784b12
William Nylund <william.nylund@gmail.com>
2020-09-18 15:09:46 +0300

    WIP cardReader, still has to parse xml into Transaction object(?)

--- src/model/CardReader.java
+++ src/model/CardReader.java
@@ -1,7 +1,17 @@
 package model;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.bind.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -84,16 +94,19 @@ public class CardReader {
             URL url = new URL("http://localhost:9002/cardreader/result");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
+            con.setRequestProperty("Accept", "application/xml");
+            InputStream xml = con.getInputStream();
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(xml);
+
+            //doc is now a Document con containing xml data
+
             con.disconnect();
-            return content.toString();
-        } catch(IOException e){
+            return "yoo";
+
+        } catch(IOException | ParserConfigurationException | SAXException e) {
             e.printStackTrace();
             return "something went terrible wrong in CardReader.getResult() :(((";
         }
