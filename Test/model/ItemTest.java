98df2f6cae02466dc9bc8b39e53e324185570db5
hmartola <64286599+hmartola@users.noreply.github.com>
2020-10-21 14:14:49 +0300

    edit tests

--- Test/model/ItemTest.java
+++ Test/model/ItemTest.java
@@ -15,8 +15,7 @@ class ItemTest {
     @Test
     void addItemAndRemoveItem() {
 
-        ProductCatalog testProductCatalog;
-        testProductCatalog = ProductCatalog.getInstance();
+        ProductCatalog testProductCatalog = ProductCatalog.getInstance();
         Transaction testTransaction = new Transaction();
 
         Item testItem = testProductCatalog.getProductByName("Banana");
@@ -31,13 +30,15 @@ class ItemTest {
         assertFalse(testTransaction.getItemList().contains(testItem), "Banana should be removed from item list");
         assertTrue(testTransaction.getItemList().contains(testItem2), "Item with barcode 1 should be in item list");
 
+        assertFalse(testProductCatalog.getCatalog().contains("nvidia RTX3090"), "RTX3090 should not be in item list because it does not exist");
+        assertFalse(testProductCatalog.getCatalog().contains(80085), "80085 should not be in item list because it does not exist");
+
     }
 
     @Test
     void verifyBestBeforeAlert() {  // BEFORE RUNNING, DISABLE THE ALERT CALL IN ADDITEM() AND CHECK THE BOOLEAN
 
-        ProductCatalog testProductCatalog;
-        testProductCatalog = ProductCatalog.getInstance();
+        ProductCatalog testProductCatalog = ProductCatalog.getInstance();
         Transaction testTransaction = new Transaction();
 
         Item oldItem = testProductCatalog.getProductByName("Milk");
@@ -53,4 +54,18 @@ class ItemTest {
         assertTrue(testTransaction.testInfoMessage == false, "The BBE alert should --NOT-- have been called");
     }
 
+    @Test
+    void correctDiscount() {
+
+        ProductCatalog testProductCatalog = ProductCatalog.getInstance();
+
+        Item item = testProductCatalog.getProductByName("Milk");
+        item.setPrice(3.50);
+        Double priceBefore = item.getPrice();
+        Double priceAfter = item.calculateDiscount(priceBefore,0.5);
+        System.out.println(priceBefore + " || " + priceAfter);
+        assertTrue(!priceBefore.equals(priceAfter), "The price should change");
+        assertTrue(priceAfter.equals(priceBefore/2), "New price should be 1.75");
+
+    }
 }
