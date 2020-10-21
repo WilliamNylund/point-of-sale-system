fc8147950d35ce62d2dc0b9a00bcb3a91738820b
rkantero <jrkantero@gmail.com>
2020-10-21 13:58:36 +0300

    add test classes

---
+++ Test/model/BonusCardTest.java
@@ -0,0 +1,78 @@
+package model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusCardTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getNumber() {
+    }
+
+    @Test
+    void setNumber() {
+    }
+
+    @Test
+    void getGoodThruMonth() {
+    }
+
+    @Test
+    void setGoodThruMonth() {
+    }
+
+    @Test
+    void getGoodThruYear() {
+    }
+
+    @Test
+    void setGoodThruYear() {
+    }
+
+    @Test
+    void getBlocked() {
+    }
+
+    @Test
+    void setBlocked() {
+    }
+
+    @Test
+    void getExpired() {
+    }
+
+    @Test
+    void setExpired() {
+    }
+
+    @Test
+    void getHolderName() {
+    }
+
+    @Test
+    void setHolderName() {
+    }
+
+    @Test
+    void getPoints() {
+    }
+
+    @Test
+    void setPoints() {
+    }
+
+    @Test
+    void addBonusPoints() {
+    }
+}
---
+++ Test/model/TransactionLogTest.java
@@ -0,0 +1,54 @@
+package model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionLogTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getPausedTransactions() {
+    }
+
+    @Test
+    void setPausedTransactions() {
+    }
+
+    @Test
+    void getCompletedTransactions() {
+    }
+
+    @Test
+    void setCompletedTransactions() {
+    }
+
+    @Test
+    void createMockTransactions() {
+    }
+
+    @Test
+    void getProductsSoldByDate() {
+    }
+
+    @Test
+    void getProductsSoldByCustomer() {
+    }
+
+    @Test
+    void sortByValue() {
+    }
+}
