e1b6f5fb33b8b6c75539ebd89d85636d0bdfa139
hmartola <64286599+hmartola@users.noreply.github.com>
2020-10-21 11:16:38 +0300

    add .bat file to run all jar files with a double-click

---
+++ pvp20_runAllJARs.bat
@@ -0,0 +1,6 @@
+@echo off
+start "ProductCatalog" java -jar ProductCatalog.jar 
+start "CustomerRegister" java -jar CustomerRegister.jar 
+start "CardReader" java -jar CardReader.jar
+start "CashBox" java -jar CashBox.jar 
+pause
